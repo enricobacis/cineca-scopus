a7df7d3522ae925b5946abd554e65eb8ac3bc93f
enricobacis <enrico.bacis@gmail.com>
2016-08-30 12:00:22 +0200

    save number of entries in table

--- src/cineca2.py
+++ src/cineca2.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
     query = 'SELECT author, group_concat(id) FROM authors GROUP BY author'
     with sqlite3.connect(DBFILE) as connection:
         connection.execute('CREATE TABLE IF NOT EXISTS articles ('
-                           'author PRIMARY KEY, data)')
+                           'author PRIMARY KEY, num, entries)')
         with closing(connection.cursor()) as cursor:
             for author, IDs in cursor.execute(query):
                 try:
                     print(Fore.CYAN + ('\n%s [%s]' % ( author, IDs)))
                     entries = sc.get_scopus_entries(IDs.split(','))
-                    connection.execute('INSERT INTO articles VALUES (?, ?)',
-                                       (author, json.dumps(entries)))
+                    connection.execute('INSERT INTO articles VALUES (?,?,?)',
+                            (author, len(entries), json.dumps(entries)))
                     print(Fore.GREEN + ('Entries: %d' % len(entries)))
                 except KeyboardInterrupt:
                     print(Fore.RED + "\nBye bye :'(")
