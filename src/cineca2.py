399a28a1ccc2e31a53a4be80290aa64b4a6129e0
enricobacis <enrico.bacis@gmail.com>
2016-08-30 11:41:23 +0200

    ensure database is correctly closed

--- src/cineca2.py
+++ src/cineca2.py
@@ -9,18 +9,19 @@ init(autoreset=True)
 from config import APIKEY, DBFILE
 
 if __name__ == '__main__':
-    try:
-        sc = ScopusClient(APIKEY)
-        query = 'SELECT author, group_concat(id) FROM authors GROUP BY author'
-        with sqlite3.connect(DBFILE) as connection:
-            connection.execute('CREATE TABLE IF NOT EXISTS articles ('
-                               'author PRIMARY KEY, data)')
-            with closing(connection.cursor()) as cursor:
-                for author, IDs in cursor.execute(query):
+    sc = ScopusClient(APIKEY)
+    query = 'SELECT author, group_concat(id) FROM authors GROUP BY author'
+    with sqlite3.connect(DBFILE) as connection:
+        connection.execute('CREATE TABLE IF NOT EXISTS articles ('
+                           'author PRIMARY KEY, data)')
+        with closing(connection.cursor()) as cursor:
+            for author, IDs in cursor.execute(query):
+                try:
                     print(Fore.BLUE + ('\n%s [%s]' % ( author, IDs)))
                     entries = sc.get_scopus_entries(IDs.split(','))
                     connection.execute('INSERT INTO articles VALUES (?, ?)',
                                        (author, json.dumps(entries)))
                     print(Fore.GREEN + ('Entries: %d' % len(entries)))
-    except KeyboardInterrupt:
-        print(Fore.RED + "\nBye bye :'(")
+                except KeyboardInterrupt:
+                    print(Fore.RED + "\nBye bye :'(")
+                    break
